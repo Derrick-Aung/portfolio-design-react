1e78a815f926713c3cf39a8ca745e287fab46b47
Derrick Aung <derek.aungkhant@protonmail.com>
2020-02-06 22:52:39 +0630

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Derrick-Aung/portfolio-design-react/precache-manifest.5d09c19fc115817cd9f023bc43f83fdc.js"
+  "/portfolio-design-react/precache-manifest.da1c768a7ebf7354acead5e4da437628.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Derrick-Aung/portfolio-design-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-design-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
