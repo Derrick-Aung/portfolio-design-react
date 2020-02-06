aa7fd856689e4f00a1caabd5535464ba3c72775a
Derrick Aung <derek.aungkhant@protonmail.com>
2020-02-06 22:26:30 +0630

    Updates

---
+++ precache-manifest.5d09c19fc115817cd9f023bc43f83fdc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31837e0940e62188d37e83f72771d38",
+    "url": "/Derrick-Aung/portfolio-design-react/index.html"
+  },
+  {
+    "revision": "5161f9a6d3c835a3f2ea",
+    "url": "/Derrick-Aung/portfolio-design-react/static/css/main.5d3de497.chunk.css"
+  },
+  {
+    "revision": "cbb52392703e53847290",
+    "url": "/Derrick-Aung/portfolio-design-react/static/js/2.1974c4ad.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Derrick-Aung/portfolio-design-react/static/js/2.1974c4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5161f9a6d3c835a3f2ea",
+    "url": "/Derrick-Aung/portfolio-design-react/static/js/main.72362da9.chunk.js"
+  },
+  {
+    "revision": "8d5703563fc5753a93be",
+    "url": "/Derrick-Aung/portfolio-design-react/static/js/runtime-main.ca5ed57c.js"
+  },
+  {
+    "revision": "5a42df19ad9d8bd2c3a196733b57b4ac",
+    "url": "/Derrick-Aung/portfolio-design-react/static/media/arduino_logo.5a42df19.png"
+  },
+  {
+    "revision": "e79e5dbde23109879fa90860aa73499c",
+    "url": "/Derrick-Aung/portfolio-design-react/static/media/c_logo.e79e5dbd.png"
+  },
+  {
+    "revision": "1399739030aea41605dfe91130605fcc",
+    "url": "/Derrick-Aung/portfolio-design-react/static/media/heartrate_sensor.13997390.jpg"
+  },
+  {
+    "revision": "cf28632239db6e02bc09bcca6bf5360f",
+    "url": "/Derrick-Aung/portfolio-design-react/static/media/matlab_logo.cf286322.png"
+  }
+]);
